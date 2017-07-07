7741a44648ee30b61df4fa5c87c4234fd1e3e276
codeamac08 <codeamac08@gmail.com>
2017-07-07 16:41:50 -0500

    modificando prueba.rb

--- lunes pre work /prueba.rb
+++ lunes pre work /prueba.rb
@@ -1,20 +1,16 @@
-Last login: Mon Jul  3 08:39:41 on console
-CodeaCamp08s-Mac-mini:~ codea_mac_08$ ls
-Api-post	Gemfile		Public		chef.db		ejemplo.db
-Applications	Gemfile.lock	README.md	chef.rb		online_store
-Desktop		Library		Rakefile	chefs.db	people.csv
-Documents	Movies		app		chefs.rb	spec
-Downloads	Music		b		config		test_maraton
-Dropbox		NavBar_Sup.css	bank		config.ru	toy_app
-Dropbox (Codea)	Pictures	bitly_rails	db		tutorial_one
-CodeaCamp08s-Mac-mini:~ codea_mac_08$ cd desktop
-CodeaCamp08s-Mac-mini:desktop codea_mac_08$ ruby prueba.rb
-What's your first name? alfredo
-What's your last name? alfredo
-What city are you from? mexico city
-What state or providence are you from? mexico city
-Your name is alfredo alfredo and you're from mexico city, mexico city
-CodeaCamp08s-Mac-mini:desktop codea_mac_08$ 
+print "What's your first name? "
+first_name = gets.chomp
+
+print "What's your last name? "
+last_name = gets.chomp
+
+print "What city are you from? "
+city = gets.chomp
+
+print "What state or providence are you from? "
+state = gets.chomp
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}"
 
 
 
